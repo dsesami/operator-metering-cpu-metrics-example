acfd72867901c360d0f9e88660935efe2b753aad
Ryan Himmelwright <ryan.himmelwright@gmail.com>
2018-11-01 20:10:38 +0000

    Mapped out stub functions of required oc replacement functions

--- Testing/test.py
+++ Testing/test.py
@@ -3,7 +3,7 @@
 import datetime
 import kubernetes
 
-# Configs can be set in Configuration class directly or using helper utility
+## Openshift OC-like functions
 
 def init():
     """Initialize connection to kubernetes"""
@@ -18,11 +18,27 @@ def get_pod_names(conn, namespace="openshift-metering"):
         if namespace in i.metadata.namespace:
             print(i.metadata.name)
 
+def new_project():
+    """Creates a new openshift project (like oc new-project)"""
+
+def run_container():
+    """Runs a container (like oc run)"""
+
+def exec_container():
+    """Executes a command in a container (like oc exec)"""
+
+def scale_deployment():
+    """Scales a deployment (like oc scale)"""
+
+
+## Testing Helper Functions
 def get_utc_timestamp():
     """Returns the current UTC Time in proper format"""
     date = datetime.datetime.utcnow()
     fmt_str = '{:04}-{:02}-{:02}T{:02}:{:02}:{:02}Z'
     return fmt_str.format(date.year, date.month, date.day, date.hour, date.minute, date.second)
+
+## Testing Wrapepr Functions
 
 print("Listing pods with their IPs:")
 get_pod_names(init())
