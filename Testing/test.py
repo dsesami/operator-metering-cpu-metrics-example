0ceb5a1f3bffc06db57c376450a471f3054e920e
Ryan Himmelwright <ryan.himmelwright@gmail.com>
2018-11-05 13:49:19 -0500

    Got new_project working, if you give it an api_instance

--- Testing/test.py
+++ Testing/test.py
@@ -1,6 +1,8 @@
 """Some functions to help with automated testing"""
 
 import datetime
+from pprint import pprint
+import openshift
 import kubernetes
 
 ## Openshift OC-like functions
@@ -18,14 +20,28 @@ def get_pod_names(conn, namespace="openshift-metering"):
         if namespace in i.metadata.namespace:
             print(i.metadata.name)
 
-def new_project():
+def new_project(api_instance, project_name, pretty="pretty_example"):
     """Creates a new openshift project (like oc new-project)"""
+    body = openshift.client.V1Project()
+    body.metadata = kubernetes.client.V1ObjectMeta(name= project_name)
+    try:
+        api_response = api_instance.create_project(body, pretty=pretty)
+        pprint(api_response)
+    except kubernetes.client.rest.ApiException as api_exception:
+        print("Exception when calling ProjectOpenshiftIoV1Api->create_project: %s\n"
+              % api_exception)
+
+def get_projects():
+    """Returns a list of the projects"""
+    
 
 def run_container():
     """Runs a container (like oc run)"""
 
+
 def exec_container():
     """Executes a command in a container (like oc exec)"""
+
 
 def scale_deployment():
     """Scales a deployment (like oc scale)"""
@@ -40,5 +56,8 @@ def get_utc_timestamp():
 
 ## Testing Wrapepr Functions
 
+
 print("Listing pods with their IPs:")
 get_pod_names(init())
+
+print()
