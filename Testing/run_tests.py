cd019a6cbf77bf81438232c5a618aafd352ab5c2
Ryan Himmelwright <ryan.himmelwright@gmail.com>
2018-11-12 16:04:56 +0000

    Added Helper fn to get list of pod names

--- Testing/run_tests.py
+++ Testing/run_tests.py
@@ -1,8 +1,8 @@
 """Functions to call oc commands"""
 
 import subprocess
-#import time
 import datetime
+import re
 import yaml
 #import random
 #import sys
@@ -26,6 +26,14 @@ def get_utc_timestamp(fmt_str='{:04}-{:02}-{:02}T{:02}:{:02}:{:02}Z'):
                                     date.minute,
                                     date.second)
     return formatted_date
+
+def get_pod_names(pod_subname, namespace='default'):
+    """Returns the full container pod names from sub-name"""
+    print(pod_subname, namespace)
+    regex = re.compile(pod_subname + r'-\d+-.{5}')
+    result = run_cmd("oc get pods -n {!s}".format(namespace))
+    match_names = regex.findall(str(result.stdout))
+    return match_names
 
 ## Test Spec functions
 def load_test_spec(spec_src):
@@ -64,9 +72,21 @@ def run_oc_steps():
 
     ## Exec Steps
 
-def run_procedure(spec, run_key="run"):
+def run_procedure(test_spec, run_key="run"):
     """Runs the run operations"""
-    return spec[run_key]
+    run_spec = test_spec[run_key]
+    for run in run_spec:
+        args = run_spec[run]
+        arg_str = args + " " if args else ''
+        cmd = "oc run {!s}{!s}".format(arg_str, run)
+        print(cmd)
+
+def exec_procedure(test_spec, exec_key="exec"):
+    """Runs the exec case"""
+    exec_spec = test_spec[exec_key]
+    for exec_call in exec_spec:
+        print(exec_call)
+
 
 def generate_report():
     """Generates the operator-metering report"""
